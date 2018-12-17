d762c2a8580800a21210617d3bd9acd241273ed5
headplus <benjiaminwu@outlook.com>
2018-12-17 11:02:39 +0800

    添加vue，parcel，完成button基本样式

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import Vue from "vue"
+import Button from "./button"
+
+Vue.component("d-button", Button)
+
+new Vue({
+    el:"#app",
+    data : {
+       
+    }
+}) 
---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button class="d-button">按钮</button>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+    .d-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        background-color: var(--button-bg);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color-hover);
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
