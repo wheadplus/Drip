886bbe048c48084ae51cfa35cac5c1b11dec3560
headplus <benjiaminwu@outlook.com>
2018-12-21 22:31:36 +0800

    更新package

---
+++ index.js
@@ -0,0 +1,7 @@
+import Button from "./button"
+import Icon from './icon'
+import ButtonGroup from './button-group'
+
+Vue.component("d-button", Button)
+Vue.component("d-icon", Icon)
+Vue.component('d-button-group', ButtonGroup)
