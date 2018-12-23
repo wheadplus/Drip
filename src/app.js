cb3945aabbf8a3b41b60874005b447bbfba76936
headplus <benjiaminwu@outlook.com>
2018-12-23 19:27:54 +0800

    完成 grid 的 span、offset 和 gutter

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from "./button"
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from "./input"
+import Row from './row'
+import Col from './col'
 
 Vue.component("d-button", Button)
 Vue.component("d-icon", Icon)
 Vue.component('d-button-group', ButtonGroup)
 Vue.component("d-input", Input)
+Vue.component("d-row", Row)
+Vue.component("d-col", Col)
 
 new Vue({
     el:"#app",
---
+++ src/row.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="row" :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px'}">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "d-row",
+        props: {
+            gutter: {
+                type: [Number, String]
+            }
+        },
+        created () {
+            console.log("row created")
+            // 没有儿子
+        },
+        mounted () {
+            console.log("row mounted")
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter
+            })
+        }
+//        var div = document.createElement("div") //created
+//        var childDiv = document.createElement('childDiv') // child created
+//        document.body.appendChild(div) // child mounted
+//        document.body.appendChild(div) //mounted
+    }
+</script>
+
+<style lang="scss" scoped>
+    .row {
+        display: flex;
+    }
+</style>
