d23be7cf4c8dab6b28fa9b453436ae0d1240d01e
headplus <benjiaminwu@outlook.com>
2018-12-25 15:26:42 +0800

    添加 layout相关组件，完成基本功能

---
+++ src/header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default  {
+        name: "drip-header"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .header {
+
+    }
+</style>
