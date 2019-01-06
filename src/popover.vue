1b1ac6b9db49508eee2f65309d1c77c41aab5ce2
headplus <benjiaminwu@outlook.com>
2019-01-06 09:47:15 +0800

    移动content，获取trigger位置，修改content位置

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="popover" @click="xxx">
-        <div class="content-wrapper" v-if="visible">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper"><slot></slot></span>
     </div>
 </template>
 
@@ -15,9 +15,33 @@
         },
         methods: {
             xxx() {
-                console.log(this.visible)
                 this.visible = !this.visible
+                console.log('切换visible')
+
+                if(this.visible === true) {
+
+                    this.$nextTick(() => {
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        console.log('添加监听器');
+                        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        console.log(width, height, top, left)
+                        this.$refs.contentWrapper.style.left = left + 'px'
+                        this.$refs.contentWrapper.style.top = top + 'px'
+                        let x = () => {
+                            this.visible = false
+                            document.removeEventListener('click', x)
+                            console.log('删除监听器');
+                            console.log('点击body关闭popover');
+
+                        }
+                        document.addEventListener('click', x)
+                    })
+                }
             }
+        },
+        mounted() {
+
+            console.log(this.$refs);
         }
     }
 </script>
@@ -28,13 +52,12 @@
         vertical-align: middle;
         position: relative;
 
-        >.content-wrapper {
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px solid red;
-            box-shadow: 0 0 3px rgba(0, 0, 0, .5);
-        }
-    }
 
+    }
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid red;
+        box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+        transform: translateY(-100%);
+    }
 </style>
