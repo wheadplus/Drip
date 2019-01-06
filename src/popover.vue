df8348bf964c32898363ed26526bc45f89f04a2d
headplus <benjiaminwu@outlook.com>
2019-01-06 19:53:30 +0800

    添加css 样式 ，三角用before，after。filter+background 去除三角形没box-shadow的bug

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper"><slot></slot></span>
+        <span ref="triggerWrapper" class="trigger-wrapper"><slot></slot></span>
     </div>
 </template>
 
@@ -21,7 +21,8 @@
                 this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
             },
             onClickDocument(e){
-                if (this.$refs.popover === e.target || this.$refs.contentWrapper.contains(e.target)) {
+                if (this.$refs.popover &&
+                    (this.$refs.popover === e.target || this.$refs.contentWrapper.contains(e.target))) {
                     return
                 }
                 this.close()
@@ -53,17 +54,43 @@
 </script>
 
 <style scoped lang="scss">
+    $border-color: #333;
+    $border-radius: 4px;
     .popover {
         display: inline-block;
         vertical-align: middle;
         position: relative;
 
-
+        >.trigger-wrapper {
+            display: inline-block;
+        }
     }
     .content-wrapper {
         position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+        border: 1px solid $border-color;
+        /*box-shadow: 0 0 3px rgba(0, 0, 0, .5);*/
+        filter:  drop-shadow(0 0 3px rgba(0, 0, 0, .5));
+        background-color: white;
         transform: translateY(-100%);
+        padding: .5em 1em;
+        margin-top: -10px;
+        max-width: 20em;
+        word-break: break-all;
+        &::before,&::after {
+            content: "";
+            display: block;
+            border: 10px solid transparent;
+            width: 0;
+            position: absolute;
+            left: 10px;
+        }
+        &::before {
+            border-top-color: black;
+            top: 100%;
+        }
+        &::after {
+            border-top-color: white;
+            top: calc(100% - 1px);
+        }
     }
 </style>
