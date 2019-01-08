a3a50bacba53de13bd2c9a19be654f877ed448e6
headplus <benjiaminwu@outlook.com>
2019-01-08 19:44:01 +0800

    collapse-item 点击，其他儿子不显示

--- src/collapse.vue
+++ src/collapse.vue
@@ -5,8 +5,19 @@
 </template>
 
 <script>
+    import Vue from  'vue'
     export default  {
-        name: "drip-collapse"
+        name: "drip-collapse",
+        data () {
+            return {
+                eventBus: new Vue()
+            }
+        },
+        provide() {
+            return {
+                eventBus: this.eventBus
+            }
+        }
     }
 </script>
 
