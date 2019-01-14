3fb990dedd98bd40020bdb0f91fb71bfd46e0bb9
headplus <benjiaminwu@outlook.com>
2019-01-14 11:14:54 +0800

    改用vue-cli构建项目

--- index.js
+++
@@ -1,7 +0,0 @@
-import Button from "./src/button"
-import Icon from './src/icon'
-import ButtonGroup from './src/button-group'
-
-Vue.component("d-button", Button)
-Vue.component("d-icon", Icon)
-Vue.component('d-button-group', ButtonGroup)
