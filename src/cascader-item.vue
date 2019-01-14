fd8242d318e9a15538eba62205b2ce190a469f69
headplus <benjiaminwu@outlook.com>
2019-01-14 14:36:26 +0800

    递归组件解决层级问题

---
+++ src/cascader-item.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="cascaderItem">
+        {{sourceItem.name}}
+        <Grip-cascader-item
+                v-if="sourceItem.children"
+                v-for="item in sourceItem.children"
+                :sourceItem="item"
+        ></Grip-cascader-item>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Grip-cascader-item",
+        props: {
+            sourceItem: {
+                type: Object
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "var";
+    .cascaderItem {
+        border:1px solid blue;
+        padding: 10px;
+    }
+</style>
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,16 +1,46 @@
 <template>
-
+    <div class="cascader">
+        <div class="trigger" @click="popoverVisible = !popoverVisible">
+            <slot></slot>
+        </div>
+        <div class="popover" v-if="popoverVisible">
+            <div v-for="item in source">
+                <cascader-item :sourceItem="item"></cascader-item>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import CascaderItem from './cascader-item.vue'
     export default {
         name: "Grip-Cascader",
+        components:{CascaderItem},
+        props: {
+            source: {
+                type: Array
+            }
+        },
+        data() {
+            return {
+                popoverVisible: false
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
     @import "var";
     .cascader {
-
+        .trigger {
+            border: 1px solid blue;
+            height: 32px;
+            width: 128px;
+        }
+        .popover {
+            border: 1px solid blue;
+            height: 128px;
+            width: 64px;
+        }
     }
 </style>
